f23803d7df86d0f074f5fbf6c4e421b679cc72cf
Choko <alexejlevochkin@gmail.com>
2025-10-21 11:19:53 +0300

    registration admin_panel +auto generation slug(base on title)

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    prepopulated_fields = {'slug': ('title')}
+
+admin.site.register(Product)
+admin.site.register(Gallery)

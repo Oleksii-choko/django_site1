24a2dfb6ebbfec9e78ea03feb28c2f6bb06a98da
Choko <alexejlevochkin@gmail.com>
2025-10-21 12:37:00 +0300

    start_project file v0.0.2 Have made admin file

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Product, Category,Gallery
+from .models import Product, Category, Gallery
+
+
+class GalleryInline(admin.TabularInline):
+    fk_name ='product'
+    model = Gallery
+    extra = 1
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'parent')
-    prepopulated_fields = {'slug': ('title')}
+    prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Product)
-admin.site.register(Gallery)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'category', 'quantity', 'price', 'created_at', 'size', 'color')
+    list_editable = ('price', 'quantity', 'size', 'color')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('title', 'price')
+    list_display_links = ('title', 'pk')
+    inlines = (GalleryInline, )
